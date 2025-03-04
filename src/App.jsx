6570c523a3bfcf83c17b34db905660c4302df3f6
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-03-05 00:54:04 +0600

    Sidebar from Metarial Tailwind with setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@material-tailwind/react";
+import Sidebar from "./Components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Sidebar />
+      </div>
+    </>
+  );
+}
+
+export default App;
