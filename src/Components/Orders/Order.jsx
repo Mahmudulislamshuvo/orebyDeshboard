c7a875ec6f9abdc4386b425fae70f54310da4c99
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-03-11 05:17:59 +0600

    order

--- src/Components/Orders/Order.jsx
+++ src/Components/Orders/Order.jsx
@@ -1,6 +1,24 @@
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+  Option,
+  Select,
+  Textarea,
+  Typography,
+  Input,
+} from "@material-tailwind/react";
 import React from "react";
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.snow.css"; // Add css for snow theme
+import Fileinput from "../Banner/Fileinput";
 
 const Order = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(!open);
+  const { quill, quillRef } = useQuill();
   return (
     <div>
       <div class="bg-white antialiased dark:bg-gray-900 md:py-2">
@@ -269,12 +287,12 @@ const Order = () => {
                       >
                         Order again
                       </button>
-                      <a
-                        href="#"
+                      <button
+                        onClick={handleOpen}
                         class="w-full inline-flex justify-center rounded-lg  border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 lg:w-auto"
                       >
                         View details
-                      </a>
+                      </button>
                     </div>
                   </div>
                 ))}
@@ -383,6 +401,98 @@ const Order = () => {
           </div>
         </div>
       </div>
+      <Dialog size="sm" open={open} handler={handleOpen} className="p-4">
+        <DialogHeader className="relative m-0 block">
+          <Typography variant="h4" color="blue-gray">
+            Manage Category
+          </Typography>
+          {/* <Typography className="mt-1 font-normal text-gray-600">
+            Keep your records up-to-date and organized.
+          </Typography> */}
+        </DialogHeader>
+        <DialogBody className="space-y-4 pb-6 max-h-[600px] overflow-y-auto">
+          <div className="flex flex-col gap-y-5 p-5 max-w-7xl mx-auto">
+            {/* Product Name */}
+            <div>
+              <Input color="blue" label="Product Name" className="w-full" />
+            </div>
+
+            {/* Quill Editor */}
+            <div className="overflow-hidden">
+              <div className="w-full h-[200px] border rounded-md shadow-sm ">
+                <div ref={quillRef} />
+              </div>
+            </div>
+
+            {/* Form Section */}
+            <div className="flex flex-col gap-x-10">
+              {/* File Input Section */}
+              <div className="w-full">
+                <div className=" bg-white rounded-lg shadow-xl">
+                  <Fileinput />
+                </div>
+              </div>
+
+              <div className="flex justify-between w-[80%] mt-5">
+                {/* Input Fields Section */}
+                <div className="flex flex-col w-1/3 gap-y-6">
+                  <div className="w-full">
+                    <Input color="blue" label="Discount" className="w-full" />
+                  </div>
+                  <div className="w-full">
+                    <Input color="blue" label="Stock" className="w-full" />
+                  </div>
+                  <div className="w-full">
+                    <Input color="blue" label="Rating" className="w-full" />
+                  </div>
+                </div>
+
+                {/* Select and Price Section */}
+                <div className="flex flex-col w-1/3 gap-y-6">
+                  <div className="w-full">
+                    <Input
+                      color="blue"
+                      label="Price"
+                      type="number"
+                      placeholder="Price"
+                      className="w-full"
+                    />
+                  </div>
+
+                  <div className="w-full">
+                    <Select label="Select Category" className="w-full">
+                      <Option>Material Tailwind HTML</Option>
+                      <Option>Material Tailwind React</Option>
+                      <Option>Material Tailwind Vue</Option>
+                      <Option>Material Tailwind Angular</Option>
+                      <Option>Material Tailwind Svelte</Option>
+                    </Select>
+                  </div>
+
+                  <div className="w-full">
+                    <Select label="Select SubCategory" className="w-full">
+                      <Option>Material Tailwind HTML</Option>
+                      <Option>Material Tailwind React</Option>
+                      <Option>Material Tailwind Vue</Option>
+                      <Option>Material Tailwind Angular</Option>
+                      <Option>Material Tailwind Svelte</Option>
+                    </Select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </DialogBody>
+
+        <DialogFooter>
+          <div className="flex gap-x-5">
+            <Button variant="filled" onClick={handleOpen} color="red">
+              Cancel
+            </Button>
+            <Button onClick={handleOpen}>Update</Button>
+          </div>
+        </DialogFooter>
+      </Dialog>
     </div>
   );
 };
